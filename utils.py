7925ab28c0962abe9094529c8fb8e48bd484cdf9
dispensable <sunsetmask@gmail.com>
2017-03-23 01:24:19 +0800

    implement set file descriptor nonblocking func, set close_on_exec func

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
 # -*- coding:utf-8 -*-
 
-def merge_cli_and_config_file(config_dict, cli_dict):
-    pass
+import fcntl
+import os
+
+
+def set_fd_nonblocking(fd):
+    flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    flags |= os.O_NONBLOCK
+    fcntl.fcntl(fcntl.F_SETFL, flags)
+
+
+def set_fd_close_on_exec(fd):
+    fcntl.fcntl(fd, fcntl.F_SETFD, 1)
