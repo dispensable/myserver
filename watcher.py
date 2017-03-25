f54ae4dbfa2c678560622f174cadfab06ad0dfdd
dispensable <sunsetmask@gmail.com>
2017-03-25 20:04:57 +0800

    manage worker implement

--- watcher.py
+++ watcher.py
@@ -8,12 +8,17 @@ import utils
 import signal
 import select
 import errno
+from setproctitle import setproctitle
+import time
 
 
 class Watcher(object):
 
     LISTENERS = []
     PIPE = []
+
+    # workers {pid: worker}
+    WORKERS = {}
 
     # signal HUP QUIT INT TERM TTIN TTOU USR1 USR2 WINCH
     # 需要排队的信号
@@ -27,6 +32,10 @@ class Watcher(object):
                signal.SIGUSR2,
                signal.SIGWINCH]
     SIGQUEUE = []
+
+    # error
+    WORKER_BOOT_ERROR_EXIT = 2
+    APP_LOAD_EXIT_ERROR = 3
 
     def __init__(self, app):
         self.app = app
@@ -75,14 +84,17 @@ class Watcher(object):
         # 为子进程退出信号安装处理器
         signal.signal(signal.SIGCHLD, self.handle_chld)
 
+        # 为gracfully退出的定时器安装处理器
+        signal.signal(signal.SIGALRM, self.handle_alrm)
+
     def signal(self, sig, frame):
         """ 将信号添加到队列中 """
         if len(self.SIGQUEUE) < 5:
             self.SIGQUEUE.append(sig)
 
-    # handler for HUP QUIT INT TERM TTIN TTOU USR1 USR2 WINCH
+    # handler for HUP QUIT INT TERM TTIN TTOU USR1 USR2 WINCH ALARM
     def handle_chld(self, sig, frame):
-        self.reap_worker()
+        self.wait_worker()
 
     def handle_hup(self):
         self.log.info("Hang up: %s ", str(self.master_name))
@@ -111,7 +123,7 @@ class Watcher(object):
 
     def handle_usr1(self):
         self.log.reopen_files()
-        self.kill_worker(signal.SIGUSR1)
+        self.kill_all_workers(signal.SIGUSR1)
 
     def handle_usr2(self):
         self.reexec()
@@ -120,10 +132,13 @@ class Watcher(object):
         if self.cfg.get('daemon'):
             self.log.info("Gracefully closing workers")
             self.num_of_workers = 0
-            self.kill_workers(signal.SIGTERM)
+            self.kill_all_workers(signal.SIGTERM)
         else:
             self.log.info("Got sigwinch but ignore, "
                           "cause this process isn't daemon")
+
+    def handle_alrm(self, sig, frame):
+        self.kill_all_workers(signal.SIGKILL)
 
     def sleep(self):
         """ use select to check if there is a signal comes """
@@ -202,10 +217,75 @@ class Watcher(object):
         pass
 
     def manage_worker(self):
-        pass
+        is_num_equal = len(self.WORKERS) - self.num_of_workers
+        if is_num_equal == 0:
+            return
+        elif is_num_equal < 0:
+            while len(self.WORKERS) < self.num_of_workers:
+                self.spwnworker()
+        elif is_num_equal > 0:
+            sorted_workers = sorted(self.WORKERS.items(), key=lambda workers: workers[1].age)
+            while len(self.WORKERS) < self.num_of_workers:
+                oldest_worker = self.WORKERS.pop(sorted_workers[0][0])
+                self.kill_worker(oldest_worker.pid, signal.SIGTERM)
+
+        active_worker_num = len(self.WORKERS)
+        if self._last_logged_active_worker_count != active_worker_num:
+            self._last_logged_active_worker_count = active_worker_num
+            self.log.info("%s worker(s) active", str(active_worker_num))
+
+    def spwnworker(self):
+
+        # 实例化worker
+        self.worker_age += 1
+        Worker_Class = self.cfg.get("worker_class")
+        worker = Worker_Class(self.worker_age, self.pid, self.app, self.log, self.LISTENERS, self.timeout / 2)
+
+        # worker fork hook
+        self.cfg.get('before_fork_worker')()
+
+        # fork
+        childpid = os.fork()
+
+        # watcher进程
+        if childpid != 0:
+            self.WORKERS[childpid] = worker
+            self.num_of_workers += 1
+            return childpid
+
+        # worker进程
+        if childpid == 0:
+            try:
+                # 设置进程名称 参考：https://pypi.python.org/pypi/setproctitle 使用该库
+                setproctitle(self.cfg.get('default_proc_name') + ' worker {}'.format(childpid))
+
+                worker.pid = os.getpid()
+                self.log.info("worker running with pid %s", childpid)
+                self.cfg.get('after_worker_fork')(self, worker)
+                # pre fork
+                worker.init()
+                sys.exit(0)
+            except Exception as e:
+                raise e
+            finally:
+                self.log.info("worker %s booted", str(childpid))
+                try:
+                    worker.tmp.close()
+                    self.cfg.get('after_worker_exit_hook')(self, worker)
+                except Exception as e:
+                    self.log.warning("During worker %s exit, error  %s happend.", str(childpid), str(e))
 
     def reload(self):
         pass
+
+    def kill_timeout_worker(self):
+        timeout = self.cfg.get('timeout')
+
+        if timeout:
+            for pid, worker in self.WORKERS.items():
+                if time.time() - worker.tmp.last_update > timeout:
+                    self.kill_worker(pid, signal.SIGKILL)
+        return
 
     def run(self):
         self.start()
@@ -214,17 +294,61 @@ class Watcher(object):
 
         print('watcher is running...')
 
-    def reap_worker(self):
-        pass
+    def wait_worker(self):
+        """ wait子进程，防止僵尸进程 """
+        try:
+            # 回收子进程
+            while True:
+                childid, status = os.waitpid(-1, os.WNOHANG)
 
-    def stop(self):
-        pass
+                if childid <= 0:
+                    break
+                else:
+                    # 正常终止 15-8 为退出状态 7-0为0
+                    if os.WIFEXITED(status):
+                        exit_code = os.WEXITSTATUS(status)
+                        if exit_code == self.WORKER_BOOT_ERROR_EXIT:
+                            pass
+                        if exit_code == self.APP_LOAD_EXIT_ERROR:
+                            pass
+                    worker = self.WORKERS.pop(childid, None)
+                    if not worker:
+                        continue
+                    worker.tmp.close()
+                    self.cfg.get('after_child_exit_hook')(self, worker)
+        except OSError as e:
+            if e.args[0] != errno.ECHILD:
+                raise
 
-    def kill_worker(self, sig):
-        pass
+    def stop(self, gracefully=True):
+        # 关闭套接字
+        for listener in self.LISTENERS:
+            listener.close()
 
-    def kill_workers(self, sig):
-        pass
+        if gracefully:
+            # 设置定时器 超时后将kill all worker
+            signal.alarm(self.cfg.get("graceful_timeout"))
+            self.kill_all_workers(signal.SIGTERM)
+            signal.alarm(0)
+        else:
+            self.kill_all_workers(signal.SIGKILL)
+
+    def kill_worker(self, pid, sig):
+        try:
+            os.kill(pid, sig)
+        except OSError as e:
+            # 无此进程
+            if e.args[0] == errno.ESRCH:
+                worker = self.WORKERS.pop(pid, None)
+                if worker:
+                    worker.tmp.close()
+                    self.cfg.get('after_worker_exit_hook')(self, worker)
+                    return
+            raise
+
+    def kill_all_workers(self, sig):
+        for pid in self.WORKERS.keys():
+            self.kill_worker(pid, sig)
 
     def reexec(self):
         pass
