8fdb9d2cf71c402161fa5be6931e5ed9272ab784
dispensable <sunsetmask@gmail.com>
2017-04-22 20:45:06 +0800

    add a lot of method to response and request class utils add helper function routin bug fix

--- webframework/request.py
+++ webframework/request.py
@@ -1,6 +1,10 @@
 from .utils import LocalVar
 from http.cookies import SimpleCookie
-
+from urllib import parse
+from json import load, JSONDecodeError
+from io import BytesIO
+from cgi import FieldStorage
+from .utils import FileUpload
 
 class Request(object):
     """ A WSGI environ warper so we can easily access request property"""
@@ -24,7 +28,7 @@ class Request(object):
         return headers
 
     @property
-    def cookies(self):
+    def cookie(self):
         """ 返回SIMPLECOOKIE生成的SimpleCookie实例"""
         return SimpleCookie(self.environ['HTTP_COOKIE'])
 
@@ -37,8 +41,50 @@ class Request(object):
         return self.environ['QUERY_STRING']
 
     @property
+    def parsed_url(self):
+        return parse.urlparse(self.url)
+
+    @property
+    def query(self):
+        """ 返回一个query字典 形式如 {'q': ['a'],...}
+            [] 用来指定多次指定相同参数的情况
+        """
+        return parse.parse_qs(self.query_str)
+
+    @property
+    def environ(self):
+        """ 返回一个wsgi environ字典的副本 """
+        return self.environ.copy()
+
+    @property
+    def json(self):
+        json_header = self.environ.get('HTTP_CONTENT_TYPE', '').lower()
+        if ('application/json' in json_header) or \
+            ('application/json-rpc' in json_header):
+            try:
+                return load(self.body)
+            except TypeError as e:
+                # TODO: 替换为400错误
+                raise e
+            except JSONDecodeError as e:
+                raise e
+        raise AttributeError('Not json body !')
+
+    @property
     def body(self):
-        return self.environ['wsgi.input']
+        """ 返回一个 BytesIO 对象， 后续访问只seek（0）后返回"""
+        body = self.environ['wsgi.input']
+        # 非第一次读取
+        if isinstance(body, BytesIO):
+            body.seek(0)
+            return body
+        # 第一次读取，替换input为BoytesIO对象
+        else:
+            body_data = body.read()
+            self.environ['wsgi.input'] = BytesIO()
+            body = self.environ['wsgi.input']
+            body.write(body_data)
+            return body
 
     @property
     def chunked(self):
@@ -77,7 +123,59 @@ class Request(object):
         if auth:
             return auth
 
+    @property
+    def content_type(self):
+        return self.environ.get('HTTP_CONTENT_TYPE', '')
+
+    @property
+    def form(self):
+        """ use cgi fieldstorage 解析 multipart body and return a FileStorage object"""
+        if self.method.upper() == 'POST' and self.content_type.upper().startswith('MULTIPART/'):
+            form = FieldStorage(fp=self.body, environ=self.environ, keep_blank_values=True)
+            return form
+        else:
+            raise AttributeError("Can not parse form data. May form not exists.")
+
+    @property
+    def files(self):
+        """ from forms parse FileUpload file instance and
+         return a dict {filename: FileUpload instance}"""
+        try:
+            field_list = self.form.list()
+        except AttributeError:
+            raise
+
+        files = {}
+        field_list = field_list or []
+        for field in field_list:
+            if field.filename:
+                files[field.name] = FileUpload(field.file, field.filename,
+                                               field.name, field.headers)
+
+        return files
+
+    def get_form_value(self, value, default=None):
+        """
+        get form value from forms
+        :param value: form name
+        :param default: if form name field not exists, use this default value to avoid error
+        :return: FieldStorage instance or MiniFieldStorage instance with these attrs in f:
+        f.name -> field name
+        f.filename -> client filesystem filename when upload it
+        f.value -> string value
+        f.file -> filelike object
+        f.type -> content type
+        f.type_options -> 在HTTP请求行的内容类型行指定的选项字典
+        f.headers -> headers
+        see: https://hg.python.org/cpython/file/2.7/Lib/cgi.py
+        """
+        try:
+            return self.form.getvalue(value, default)
+        except AttributeError:
+            raise
+
 
 class RequestWrapper(Request):
     environ = LocalVar()
     init = Request.__init__
+
--- webframework/utils.py
+++ webframework/utils.py
@@ -1,6 +1,12 @@
 # -*- coding:utf-8 -*-
 
 from threading import local
+import os.path
+import http.client
+import hashlib
+import base64
+import pickle
+import hmac
 
 
 class LocalVar(object):
@@ -9,12 +15,66 @@ class LocalVar(object):
 
     def __get__(self, instance, owner):
         try:
-            return self.local_var.var
+            return self.local_var
         except ValueError:
             raise ValueError('context not initialized')
 
     def __set__(self, instance, value):
-        self.local_var.var = value
+        self.local_var = value
 
     def __delete__(self, instance):
-        del self.local_var.var
+        del self.local_var
+
+
+class FileUpload:
+    def __init__(self, fileobj, filename, formname, headers=None):
+        self.file = fileobj # BytesIO or temp file
+        self.filename = filename
+        self.formname = formname
+        self.headers = headers if headers else {}
+
+    def get_header(self, name, default=None):
+        return self.headers.get(name, default)
+
+    def filename(self):
+        return self.filename
+
+    def _copy_file(self, tofile, chunk_size=2**16):
+        file_tell = self.file.tell()
+        while True:
+            data = self.file.read(chunk_size)
+            if not data:
+                break
+            tofile.write(data)
+        self.file.seek(file_tell)
+
+    def save(self, dest, overwrite=False, chunk_size=2 ** 16):
+        if isinstance(dest, str):
+            if os.path.isdir(dest):
+                dest = os.path.join(dest, self.filename)
+            if not overwrite and os.path.exists(dest):
+                raise IOError('File exists')
+            with open(dest, 'wb') as f:
+                self._copy_file(f, chunk_size)
+        elif hasattr(dest, 'write'):
+            self._copy_file(dest, chunk_size)
+        else:
+            raise TypeError('Can not save to this obj: {!s}'.format(dest))
+
+
+HTTP_CODES = http.client.responses.copy()
+HTTP_CODES[418] = "I'm a teapot"
+HTTP_CODES[428] = "Precondition Required"
+HTTP_CODES[429] = "Too Many Requests"
+HTTP_CODES[431] = "Request Header Fields Too Large"
+HTTP_CODES[511] = "Network Authentication Required"
+_HTTP_STATUS_LINES = dict((k, '%d %s' % (k, v))
+                          for (k, v) in HTTP_CODES.items())
+
+
+def sig_cookie(name, value, secret_key, secret_level=hashlib.sha256):
+    msg = base64.b64encode(pickle.dumps([name, value], -1))
+    sig = base64.b64encode(
+        hmac.new(secret_key.encode(), msg, digestmod=secret_level).digest()
+    )
+    return b''.join([b'|', sig, b'?', msg]).decode()
