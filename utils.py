840b4c4c083951b29aa2afbe767848ae76ad107c
dispensable <sunsetmask@gmail.com>
2017-04-22 23:05:12 +0800

    fix 200 200 ok 和 200 没有status phrase的bug

--- utils.py
+++ utils.py
@@ -15,15 +15,15 @@ class LocalVar(object):
 
     def __get__(self, instance, owner):
         try:
-            return self.local_var
+            return self.local_var.var
         except ValueError:
             raise ValueError('context not initialized')
 
     def __set__(self, instance, value):
-        self.local_var = value
+        self.local_var.var = value
 
     def __delete__(self, instance):
-        del self.local_var
+        del self.local_var.var
 
 
 class FileUpload:
