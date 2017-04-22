bb6dca7b4cc38c2f5d85c311f2836afa3a4e3a2a
dispensable <sunsetmask@gmail.com>
2017-04-22 20:42:08 +0800

    cookie test error test

--- app/wsgiapp.py
+++ app/wsgiapp.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 
-from webframework.myframwork import MyApp, request, response
+from webframework.myframwork import MyApp, request, response, error
 
 wsgi_app = MyApp()
 
@@ -8,14 +8,23 @@ wsgi_app = MyApp()
 @wsgi_app.route('/')
 def hello():
     response.status_code = 200
-    response.status = str(response.status_code) + ' OK'
+    response.status = 'OK'
     response.add_header('Content-Length', '11')
-    return 'hello world'
+    if request.cookie is None:
+        response.set_cookie('wang', 'test', secret_key='wang')
+    else:
+        print(request.cookie)
+    return '<h1>hello world!</h1>'
 
 
 @wsgi_app.route(r'/wiki/(?P<name>[A-Za-z0-9_]+)')
 def test(name):
     response.status_code = 200
-    response.status = str(response.status_code) + ' OK'
+    response.status = 'OK'
     response.add_header('Content-Length', '15')
     return 'hello world' + name
+
+
+@wsgi_app.route(r'/test')
+def test1():
+    return error(404)
