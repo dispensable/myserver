3ef9b3a5559d4412a45233933687949df49406c5
dispensable <sunsetmask@gmail.com>
2017-04-29 22:28:24 +0800

    error class

---
+++ error.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class HaltServer(BaseException):
+    def __init__(self, reason, exit_status=1):
+        self.reason = reason
+        self.exit_status = exit_status
+
+    def __str__(self):
+        return "<HaltServer %r %d>" % (self.reason, self.exit_status)
+
+
+class ConfigError(Exception):
+    """ Exception raised on config error """
+
+
+class AppImportError(Exception):
+    """ Exception raised when loading an application """
