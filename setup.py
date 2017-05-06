c49c39b50cc0aead1472bf35ea6c455500fb6b57
dispensable <sunsetmask@gmail.com>
2017-05-07 00:30:00 +0800

    complete setup.py information

--- setup.py
+++ setup.py
@@ -12,4 +12,14 @@ setup(
     entry_points={
         'console_scripts': ['myserver=myserver.main:run']
         },
+    install_requires=['docopt>=0.6.2', 'inotify>=0.2.8', 'Mako>=1.0.6',
+                      'pysendfile>=2.0.1', 'schema>=0.6.5', 'setproctitle>=1.1.10'],
+
+    # metadata for upload to PyPI
+    author="dispensable",
+    author_email="sunsetmask@gmail.com",
+    description="A pre-forked unix http web server",
+    license="",
+    keywords="http server",
+    url="https://dispensable.github.io/myserver/",
 )
